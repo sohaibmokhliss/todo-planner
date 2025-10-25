c29d62bd8d0d8bd272a626b2cc809f21e6b54b0f
Sohaib Mokhliss <sohaibmokhliss@gmail.com>
2025-10-25 09:18:24 +0100

    added a logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Todo Planner',
   description: 'A modern to-do & personal planning web app',
+  icons: {
+    icon: '/icon.svg',
+  },
 }
 
 export default function RootLayout({
