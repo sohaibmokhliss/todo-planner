12b211b68e487a1419ce5000ff1800e410761758
sohaib mokhliss <sohaibmokhliss@gmail.com>
2026-04-03 16:21:39 +0100

    Fix production path alias resolution

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+import path from 'path'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    }
+
+    return config
+  },
 }
 
 export default nextConfig
